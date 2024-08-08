16ee89d8762235e93b02537c5f51a690d85430a2
SangeethaKombin <sangeethakombin@gmail.com>
2024-08-08 09:58:05 +0530

    Add files via upload

---
+++ datastory.sql
@@ -0,0 +1,14 @@
+show tables;
+select * from country;
+
+
+select * from customers;
+
+
+select * from dept;
+
+select * from emp;
+
+select * from orders;
+select * from products;
+select * from salgrade; 
